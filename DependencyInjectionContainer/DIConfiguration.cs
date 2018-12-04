5a90d43e66155bbcf2fcc01de50eaff447346c62
andredance <andrey99.09@gmail.com>
2018-12-04 23:48:23 +0300

    implemented class Configuration for container

--- DependencyInjectionContainer/DIConfiguration.cs
+++ DependencyInjectionContainer/DIConfiguration.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace DependencyInjectionContainer
 {
-    class DIConfiguration
+    public class DIConfiguration
     {
+        internal Dictionary<Type, List<Type>> DContainer { get; set; }
+        internal Dictionary<Type, bool> IsSingleton { get; set; }
+        internal Dictionary<Type, SingletonContainer> SContainer { get; set; }
+
+        public void Register<TDependency, TImplementation>(bool isSingleton)
+        {
+            Type tDependency = typeof(TDependency);
+            Type tImplementation = typeof(TImplementation);
+
+            if (!DContainer.ContainsKey(tDependency))
+            {
+                DContainer[tDependency] = new List<Type>();
+            }
+
+            if (!DContainer[tDependency].Contains(tImplementation))
+            {
+                DContainer[tDependency].Add(tImplementation);
+            }
+
+            IsSingleton[tDependency] = isSingleton;
+            
+            if (IsSingleton[tDependency])
+            {
+                SContainer[tDependency] = new SingletonContainer();
+            }
+        }
+
+        public void Register(Type tDependency, Type tImplementation, bool isSingleton)
+        {
+            if (!DContainer.ContainsKey(tDependency))
+            {
+                DContainer[tDependency] = new List<Type>();
+            }
+
+            if (!DContainer[tDependency].Contains(tImplementation))
+            {
+                DContainer[tDependency].Add(tImplementation);
+            }
+
+            IsSingleton[tDependency] = false;
+        }
+
+        public DIConfiguration()
+        {
+            DContainer = new Dictionary<Type, List<Type>>();
+            SContainer = new Dictionary<Type, SingletonContainer>();
+            IsSingleton = new Dictionary<Type, bool>();
+        }
     }
 }
