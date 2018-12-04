7ed89b9b55488caeec5c5e67c3a7e67836674c65
andredance <andrey99.09@gmail.com>
2018-12-04 16:19:05 +0300

    Container for singleton object

---
+++ DependencyInjectionContainer/SingletonContainer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionContainer
+{
+    public class SingletonContainer
+    {
+        private readonly object syncRoot = new object();
+        private volatile object instance;
+        private Type type;
+
+        public SingletonContainer(Type type)
+        {
+            this.type = type;
+            instance = null;
+        }
+
+        public object GetInstance(DIProvider provider)
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                        instance = provider.Resolve(type);
+                }
+            }
+            return instance;
+        }
+    }
+}
